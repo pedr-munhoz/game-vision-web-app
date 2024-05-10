f569b4830d1f7faab072406359677664636ba260
Pedro Munhoz <pedro.munhoz@gmail.com>
2024-05-09 22:49:54 -0300

    layout adjustments

--- layouts/default.vue
+++ layouts/default.vue
@@ -18,7 +18,7 @@
         </v-list-item>
       </v-list>
     </v-navigation-drawer>
-    <v-app-bar :clipped-left="clipped" fixed app>
+    <v-app-bar :clipped-left="clipped" fixed app dense>
       <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
       <v-toolbar-title v-text="title" />
       <v-spacer />
