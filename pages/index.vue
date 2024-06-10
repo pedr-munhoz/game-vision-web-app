c65b3520fb9ef9526568cfd5aad5032a32b38089
Pedro Munhoz <pedro.munhoz@gmail.com>
2024-06-09 22:56:37 -0300

    update home page

--- pages/index.vue
+++ pages/index.vue
@@ -7,68 +7,23 @@
       </v-card>
       <v-card>
         <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
+          Welcome to the Game Vision plataform!
         </v-card-title>
         <v-card-text>
           <p>
-            Vuetify is a progressive Material Design component framework for
-            Vue.js. It was designed to empower developers to create amazing
-            applications.
-          </p>
-          <p>
-            For more information on Vuetify, check out the
-            <a
-              href="https://vuetifyjs.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              documentation </a
-            >.
-          </p>
-          <p>
-            If you have questions, please join the official
-            <a
-              href="https://chat.vuetifyjs.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="chat"
-            >
-              discord </a
-            >.
-          </p>
-          <p>
-            Find a bug? Report it on the github
-            <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="contribute"
-            >
-              issue board </a
-            >.
-          </p>
-          <p>
-            Thank you for developing with Vuetify and I look forward to bringing
-            more exciting features in the future.
+            Game Vision is a video analysis plataform for football and flag football.
+            It was designed to empower teams and athletes to take the game to the next level.
           </p>
           <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
+            <em><small>&mdash; Pedro Munhoz</small></em>
           </div>
           <hr class="my-3" />
-          <a
-            href="https://nuxtjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt Documentation
+          <a href="https://github.com/pedr-munhoz/game-vision-web-api" target="_blank" rel="noopener noreferrer">
+            Server GitHub
           </a>
           <br />
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt GitHub
+          <a href="https://github.com/pedr-munhoz/game-vision-web-app" target="_blank" rel="noopener noreferrer">
+            Web App GitHub
           </a>
         </v-card-text>
       </v-card>
